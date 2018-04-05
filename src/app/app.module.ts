2a2451453096dc5c81ee2b0d33d1753708b4f73c
ajmalmajeed <32757909+ajmalmajeed@users.noreply.github.com>
2018-04-06 02:05:00 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,43 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
-import { StatusBar } from '@ionic-native/status-bar';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { RegisterPage } from '../pages/register/register';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { firebaseConfig } from '../app/config';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    RegisterPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig.fire),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    RegisterPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth
   ]
 })
 export class AppModule {}
---
+++ src/app/config.ts
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+    fire: {
+  
+  apiKey: "AIzaSyCzEDgOMPPldTJSjXrxt-1sqbL9tJI_jzM",
+  authDomain: "data-entry-app-375c6.firebaseapp.com",
+  databaseURL: "https://data-entry-app-375c6.firebaseio.com",
+  projectId: "data-entry-app-375c6",
+  storageBucket: "data-entry-app-375c6.appspot.com",
+  messagingSenderId: "972841208362"
+  
+    }
+  };
+  
